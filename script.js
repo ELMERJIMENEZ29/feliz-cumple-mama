29db2fd9e92e7f564e73f18826b77cfdc68e06c9
Elmer Jimenez <eldagiar_29@hotmail.com>
2025-04-23 01:40:48 -0500

    RESULTADO FINAL

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const curtain = document.getElementById('curtain');
 const mainContent = document.getElementById('main-content');
 const poemText = document.getElementById('poem-text');
 const music = document.getElementById('background-music');
+const heartsContainer = document.getElementById('hearts-container');
 
 const poem = `Madre querida, tu amor es mi guía,
 Eres mi fuerza, mi alegría.
@@ -12,7 +13,13 @@ Tu sonrisa ilumina mi andar.
 Hoy te celebro con emoción,
 Gracias, mamá, por tu bendición.
 Te amo con todo mi corazón,
-¡Feliz cumpleaños, mi inspiración!`;
+¡Feliz cumpleaños, mi inspiración!
+Gracias por ser como eres y por
+nunca dejarme solo, eres simplemente 
+maravillosa, no olvides nunca que 
+TE AMO DEMASIADO MAMITA PRECIOSA,
+tampoco olvides lo valiosa que eres para mí,
+y lo importante que eres en mi vida.`;
 
 let images = document.querySelectorAll('.carousel-container img');
 let currentIndex = 0;
@@ -46,3 +53,24 @@ function prevImage() {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     showImage(currentIndex);
 }
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = (5 + Math.random() * 5) + 's';
+    heartsContainer.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 10000);
+}
+
+setInterval(createHeart, 500);
+
+
+// Abrir telón al presionar el botón
+document.getElementById('open-button').addEventListener('click', function() {
+    document.getElementById('curtain').classList.add('open');
+    document.getElementById('main-content').style.display = 'block';
+    document.getElementById('background-music').play();
+});
